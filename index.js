f5c941c79f62c795eb346d0a406bb19ee747e22e
EzequielMedina <112876@tecnicatura.frc.utn.edu.ar>
2022-04-26 15:09:59 -0300

    prueba3

---
+++ index.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+    $("#formPrincipal").validate({
+        rules:{
+            nombrecompleto:{
+                required: true,
+                minlength: 6,
+            },
+            email:{
+                required: true,
+                email:true,
+
+            },
+            confirmarEmail:{
+                required: true,
+                email: true,
+                equalTo: "#email"
+            }
+        },
+        messages:{
+            nombrecompleto:{
+                required: "por favor ingrese un nombre",
+                minlength: "minimo 6 caracteres"
+            },
+            email:{
+                required: "se requiere email",
+                email: "no es un email valido"
+            },
+            confirmarEmail:{
+                required: "no ingreso ningun email",
+                equalTo: "no coincide email"
+            }
+        },
+        errorElement: "em"
+
+    });
+
+
+});
+
+$.validator.setDefaults({
+    submitHandler: function(){
+        alert("exitoso")
+        console.log("anda")
+    }
+})
